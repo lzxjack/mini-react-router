6c54598d8992f167a51aa1bc4355b98fe23acdb1
Jack <965555169@qq.com>
2022-01-07 19:32:16 +0800

    update

--- src/router/Switch.jsx
+++ src/router/Switch.jsx
@@ -3,16 +3,20 @@ import { RouterContext } from './Router';
 
 const Switch = ({ children }) => {
     const { location } = useContext(RouterContext);
-    children = Array.isArray(children) ? children : [children];
-    for (const child of children) {
-        const {
-            props: { path },
-        } = child;
-        if (location.pathname === path) {
-            return child;
-        }
-    }
-    return null;
+    const routes = [...children];
+    return (
+        <>
+            {routes.map(child => {
+                const {
+                    props: { path },
+                } = child;
+                if (location.pathname === path) {
+                    return child;
+                }
+                return null;
+            })}
+        </>
+    );
 };
 
 export default Switch;
